570d2fdd92b4f60b1bc52c84d068e9ac2b06d048
MikaelDus <Mikael01.dusaussoy@gmail.com>
2021-04-06 15:01:20 +0200

    Exo API

--- developperDesComposantsdInterface/developperLaPartieBack_end/app/back/src/Controller/APIController.php
+++ developperDesComposantsdInterface/developperLaPartieBack_end/app/back/src/Controller/APIController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -16,7 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class APIController extends AbstractController
 {
     /**
-     * @Route("/articles/liste", name="liste", methods={"GET"})
+     * @Route("/article/liste", name="liste", methods={"GET"})
      */
     public function liste(ArticleRepository $articlesRepo)
     {
@@ -48,4 +50,94 @@ class APIController extends AbstractController
         // On envoie la réponse
         return $response;
     }
+
+    /**
+     * @Route("/article/lire/{id}", name="article", methods={"GET"})
+     */
+    public function getArticle(Article $article)
+    {
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+        $jsonContent = $serializer->serialize($article, 'json', [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]);
+        $response = new Response($jsonContent);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    /**
+     * @Route("/article/ajout", name="ajout", methods={"POST"})
+     */
+    public function addArticle(Request $request)
+    {
+
+        $article = new Article();
+
+        // On décode les données envoyées
+        $donnees = json_decode($request->getContent());
+
+        // On hydrate l'objet
+        $article->setId($donnees->id);
+        $article->setDescription($donnees->description);
+
+        // On sauvegarde en base
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        // On retourne la confirmation
+        return new Response('ok', 201);
+
+    }
+
+    /**
+     * @Route("/article/editer/{id}", name="edit", methods={"POST"})
+     */
+    public function editArticle(?Article $article, Request $request)
+    {
+
+
+        // On décode les données envoyées
+        $donnees = json_decode($request->getContent());
+
+        // On initialise le code de réponse
+        $code = 200;
+
+        // Si l'article n'est pas trouvé
+        if (!$article) {
+            // On instancie un nouvel article
+            $article = new Article();
+            // On change le code de réponse
+            $code = 201;
+        }
+
+        // On hydrate l'objet
+        $article->setId($donnees->id);
+        $article->setDescription($donnees->description);
+
+
+        // On sauvegarde en base
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        // On retourne la confirmation
+        return new Response('ok', $code);
+    }
+
+    /**
+     * @Route("/article/supprimer/{id}", name="supprime", methods={"DELETE"})
+     */
+    public function removeArticle(Article $article)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($article);
+        $entityManager->flush();
+        return new Response('ok');
+    }
+
 }
--- developperDesComposantsdInterface/developperLaPartieBack_end/app/back/src/Entity/Article.php
+++ developperDesComposantsdInterface/developperLaPartieBack_end/app/back/src/Entity/Article.php
@@ -27,8 +27,21 @@ class Article
         return $this->id;
     }
 
+    public function setId(?string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
 
     public function getDescription(): ?string {
         return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
     }
 }
